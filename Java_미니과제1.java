748ac966a2f064bdd21473deb81ebc312007cfff
namsubin26 <143942792+namsubin26@users.noreply.github.com>
2023-09-03 22:28:29 +0900

    1주차 미니과제1
    남수빈

---
+++ Java_미니과제1.java
@@ -0,0 +1,13 @@
+public class Java_미니과제1 {
+        public static void main(String[] args) {
+            System.out.println("[구구단 출력]");
+            for (int i = 1; i < 10; i++) {
+                for (int j = 1; j < 10; j++) {
+                    System.out.println( "0" + i + " X " + "0" + j + " = " + i*j);
+                }
+                System.out.println();
+
+            }
+        }
+
+}
